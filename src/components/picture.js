83470ffe881a86cec35122a045ae05320df0d19c
Rines.io <tsrines@gmail.com>
2020-05-18 19:36:34 -0400

    picture

--- src/components/picture.js
+++ src/components/picture.js
@@ -9,5 +9,5 @@ export const Picture = () => {
       "margin-right": "auto",
       "width": "15%",
       "height": "15%"}} 
-      src="public/TimHeadshot.jpg"  />
+      src="https://media-exp1.licdn.com/dms/image/C4E03AQE4GsW9EANqIg/profile-displayphoto-shrink_400_400/0?e=1595462400&v=beta&t=hL-1Au7zqrZPR6OXEPJGNHnIAdsg4xDBqAjnEgT7cOA"  />
 }
