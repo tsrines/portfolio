b1df71a467b958ffcb3fcb1b104d1ca97c16195f
Rines.io <tsrines@gmail.com>
2020-05-20 08:48:48 -0400

    travisci

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, {  } from 'react';
+import React, { } from 'react';
 // import { Link, Switch, Route, Path } from 'react-router'
 // import { BrowserRouter as Router } from 'react-router-dom'
 // import { ButtonAppBar } from './components/nav'
 import { Picture } from './components/picture'
-import './App.css'  
+import './App.css'
 
-function App() {  
+function App() {
   return (
     <div className="App">
       <Picture />
@@ -14,3 +14,32 @@ function App() {
 }
 
 export default App;
+
+
+class Solution {
+  solve(nums) {
+    let dummyArray = []
+    let result = 0
+    nums.forEach(num => {
+      for(let tester of nums){
+        if(Math.abs(num) === tester) {
+          dummyArray.push(num)
+        }
+      }
+    })
+    dummyArray.forEach(num => {
+      if(num > result) {
+        result = num
+      }
+    })
+      return result
+  }
+    
+}
+    
+
+
+      
+
+
+
