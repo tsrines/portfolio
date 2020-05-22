14b3b95150a05c19a36f69a7e4c62317b39832ea
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-22 14:09:17 +0000

    Deploy tsrines/portfolio to github.com/tsrines/portfolio.git:gh-pages

---
+++ precache-manifest.7dda2c5634ebbb9d9a67d99ec7eecc16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555a3ec50bd253e5f71cac9aa2935de6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "334754ee79c534a9c562",
+    "url": "/static/css/main.6fe980eb.chunk.css"
+  },
+  {
+    "revision": "75bb5ccd585474e0635f",
+    "url": "/static/js/2.564e0eaa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.564e0eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "334754ee79c534a9c562",
+    "url": "/static/js/main.cc0979e5.chunk.js"
+  },
+  {
+    "revision": "dcd3454aa18f0f78a518",
+    "url": "/static/js/runtime-main.d7bc6b29.js"
+  }
+]);
