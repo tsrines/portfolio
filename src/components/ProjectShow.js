c0df8e6905a137daa26ee44d765d7c9407875ffb
Rines.io <tsrines@gmail.com>
2020-05-22 11:24:38 -0400

    show page - tables done, working on images

--- src/components/ProjectShow.js
+++ src/components/ProjectShow.js
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { } from 'react'
+import { Container, Table, Image } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 export const ProjectShow = (props) => {
-  const {project} = props
+  const { project } = props
   console.log(project)
-return <h1>You are at: {project.name}</h1>
-    // // <div>
-    //   {console.log(JSON.stringify(...props))}
-    //   <div>Hi
-    //  </div>
-  
-  }
+  return (
+    <Container>
+      <Image src="" alt=""></Image>
+      <Table celled definition>
+        <Table.Body >
+          <Table.Row >
+            <Table.Cell collapsing >Name: </Table.Cell>
+            <Table.Cell collapsing>{project.name}</Table.Cell>
+          </Table.Row>
+          <Table.Row >
+            <Table.Cell >Site: </Table.Cell>
+            <Table.Cell ><a href={project.websiteURL} rel="noopener noreferrer" target="_blank">{project.slug}</a></Table.Cell>
+          </Table.Row>
+          <Table.Row >
+            <Table.Cell >Description</Table.Cell>
+            <Table.Cell >{project.longDescription}</Table.Cell>
+          </Table.Row>
+          <Table.Row >
+            <Table.Cell >Technologies</Table.Cell>
+            <Table.Cell >{project.technologies}</Table.Cell>
+          </Table.Row>
+          <Table.Row >
+            <Table.Cell >Github Front End</Table.Cell>
+            <Table.Cell ><Link>{project.github.frontend}</Link></Table.Cell>
+          </Table.Row>
+          {project.github.backend !== "" &&
+            <Table.Row >
+              <Table.Cell >Github Backend</Table.Cell>
+              <Table.Cell ><Link>{project.github.backend}</Link></Table.Cell>
+            </Table.Row>}
+
+        </Table.Body>
+      </Table>
+    </Container>
+
+
+
+  )
+
+}
 
