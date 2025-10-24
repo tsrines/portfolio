413a868549c41e192a75f628529ca9d4bf7e2994
Tim Rines <tim.rines@kitbash3d.com>
2025-10-23 21:55:47 -0400

    refactor: update social component to use Email instead of Twitter

--- components/social.tsx
+++ components/social.tsx
@@ -1,11 +1,11 @@
-import { Github, Twitter } from "./social-links"
+import { Github, Email } from "./social-links"
 
 export default function Social() {
   return (
     <div className="mt-4 flex justify-center">
       <div className="flex items-center gap-4">
         <Github />
-        <Twitter />
+        <Email />
       </div>
     </div>
   )
